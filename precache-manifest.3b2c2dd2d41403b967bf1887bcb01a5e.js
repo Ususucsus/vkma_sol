cffd7441f8027838abae05a461037eb5cbee461b
Ususucsus <usarti01@gmail.com>
2020-06-18 20:38:29 +0300

    Updates

---
+++ precache-manifest.3b2c2dd2d41403b967bf1887bcb01a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822769d8ea1175a7ba4bf0c618f0434b",
+    "url": "/vkma_sol/index.html"
+  },
+  {
+    "revision": "61004f5c040d1da99349",
+    "url": "/vkma_sol/static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "61004f5c040d1da99349",
+    "url": "/vkma_sol/static/js/2.4f7426c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vkma_sol/static/js/2.4f7426c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db28d47123168e2f1cc",
+    "url": "/vkma_sol/static/js/main.78c9d5fc.chunk.js"
+  },
+  {
+    "revision": "ea18a72091fa0ee13065",
+    "url": "/vkma_sol/static/js/runtime-main.da3ca6d8.js"
+  }
+]);
